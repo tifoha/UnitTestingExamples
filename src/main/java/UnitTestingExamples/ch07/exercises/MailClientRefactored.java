57d88c8e443585f81d0497281cd883ffcbbf138c
tifoha <tifoha@gmail.com>
2016-10-12 19:10:00 +0300

    ch07 ex done

--- src/main/java/UnitTestingExamples/ch07/exercises/MailClientRefactored.java
+++ src/main/java/UnitTestingExamples/ch07/exercises/MailClientRefactored.java
@@ -3,28 +3,17 @@ package UnitTestingExamples.ch07.exercises;
 /**
  * Created by Vitaly on 09.10.2016.
  */
-public class MailClientRefactored extends MailClient {
-    private final EmailFactory emailFactory;
-    private final EmailService emailService;
-
-    public MailClientRefactored(EmailFactory emailFactory, EmailService emailService) {
-        this.emailFactory = emailFactory;
-        this.emailService = emailService;
-    }
-
-    @Override
+public class MailClientRefactored extends MailClient{
     public void sendEmail(String address, String title, String body) {
-        Email email = emailFactory.create(address, title, body);
-        emailService.sendEmail(email);
+        Email email = getEmail(address, title, body);
+        sendEmail(email);
     }
-}
 
-interface EmailFactory {
+    protected void sendEmail(Email email) {
+        EmailServer.sendEmail(email);
+    }
 
-    Email create(String address, String title, String body);
-}
-
-interface EmailService {
-
-    void sendEmail(Email email);
+    protected Email getEmail(String address, String title, String body) {
+        return new Email(address, title, body);
+    }
 }
--- src/test/java/UnitTestingExamples/ch07/exercises/MailClientTest.java
+++ src/test/java/UnitTestingExamples/ch07/exercises/MailClientTest.java
@@ -11,16 +11,20 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
 /**
  * Created by Vitaly on 11.10.2016.
  */
 @PrepareForTest({MailClient.class, EmailServer.class})
-//@PrepareForTest(EmailServer.class)
 @RunWith(PowerMockRunner.class)
 public class MailClientTest {
 
@@ -31,6 +35,21 @@ public class MailClientTest {
     private Email email;
     private MailClient sut;
     private ArgumentCaptor<Email> emailCaptor;
+
+    class MailClientRef extends MailClientRefactored {
+        @Override
+        protected Email getEmail(String address, String title, String body) {
+            assertThat(address).isEqualTo(ADDRESS);
+            assertThat(title).isEqualTo(TITLE);
+            assertThat(body).isEqualTo(BODY);
+            return email;
+        }
+
+        @Override
+        protected void sendEmail(Email email) {
+            assertThat(email).isEqualTo(MailClientTest.this.email);
+        }
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -79,13 +98,38 @@ public class MailClientTest {
                 .sendEmail(any(Email.class));
 
         InOrder order = inOrder(emailFactory, emailService);
+        sut = new MailClientRedesigned(emailFactory, emailService);
 
 
-        sut = new MailClientRefactored(emailFactory, emailService);
         sut.sendEmail(ADDRESS, TITLE, BODY);
 
 
         order.verify(emailFactory).create(ADDRESS, TITLE, BODY);
         order.verify(emailService).sendEmail(email);
     }
+
+    @Test
+    public void sendEmailRefactoredAndSubclass() throws Exception {
+        sut = new MailClientRef();
+
+        sut.sendEmail(ADDRESS, TITLE, BODY);
+    }
+
+    @Test
+    public void sendEmailRefactoredPartialMock() throws Exception {
+        MailClientRefactored sut = spy(new MailClientRefactored());
+        doReturn(email)
+                .when(sut)
+                .getEmail(anyString(), anyString(), anyString());
+        doNothing()
+                .when(sut)
+                .sendEmail(any(Email.class));
+
+        sut.sendEmail(ADDRESS, TITLE, BODY);
+        verify(sut).getEmail(ADDRESS, TITLE, BODY);
+        verify(sut).sendEmail(email);
+    }
+
+
+
 }
